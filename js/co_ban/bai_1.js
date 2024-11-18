da877273de33c728c1ab05707cee1bf18c4b8365
DUNGTRAILE <dung123tl@gmail.com>
2024-11-18 22:06:39 +0700

    17/11/2024
    dung

---
+++ js/co_ban/bai_1.js
@@ -0,0 +1,18 @@
+// console.log("thân dũng")
+// alert("than van dung")
+
+// let fullName = 'thân văn dũng'
+
+
+// console.log(fullName)
+// confirm('bạn là thân văn dũng')
+// prompt('bạn bao nhiêu tuổi?')
+
+
+setTimeout(function() {
+    alert('thông báo')
+}, 1000)
+
+// setInterval(function() {
+//     console.log('đây là log' + Math.random())
+// })
