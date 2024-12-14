89c62b543f2a9190a7f590b25251945f80fbee46
Thân văn Dũng <156935787+DUNGTRAILE@users.noreply.github.com>
2024-12-14 09:27:34 +0700

    dung
    dung

--- lap_trinh_font_end/RESPONSIVE/thuc-hanh-1/acsset/JavaScript/app.js
+++ lap_trinh_font_end/RESPONSIVE/thuc-hanh-1/acsset/JavaScript/app.js
@@ -1,1 +1,36 @@
-console.log('hello')
+const allMenu = document.querySelector(".all img")
+const menu = document.querySelector('.menu-header')
+const dow = document.querySelector('.download')
+// console.log(menu)
+allMenu.addEventListener('click', () => {
+    // console.log('oki')
+    let flag = allMenu.getAttribute('flag')
+    console.log(flag)
+    if(flag == 1){
+        menu.style.display = 'block'
+        allMenu.setAttribute('flag', 0)
+    }
+    else{
+        menu.style.display = 'none'
+        allMenu.setAttribute('flag', 1)
+    }
+
+})
+if(window.innerWidth <768){
+    menu.style.display = 'none'
+    dow.style.display = 'none'
+}
+
+
+window.addEventListener('resize' , () => {
+    if(window.innerWidth >= 768){
+        menu.style.display = 'block'
+        allMenu.setAttribute('flag', 0)
+        dow.style.display = 'block'
+    }
+    if(window.innerWidth <768){
+        menu.style.display = 'none'
+        allMenu.setAttribute('flag', 1)
+        dow.style.display = 'none'
+    }
+})
